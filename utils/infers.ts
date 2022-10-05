d1cc5322c02c42b8f54718ce38d8d438752dadbf
julian <julian.costinea@gmail.com>
2022-10-05 17:04:04 +0200

    more infers

--- utils/infers.ts
+++ utils/infers.ts
@@ -9,8 +9,8 @@ import { Equal } from "@type-challenges/utils";
 //recursive MyAwaited for multiple promises
 type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer V>
   ? V extends Promise<unknown>
-    ? MyAwaited<V>
-    : V
+  ? MyAwaited<V>
+  : V
   : never;
 
 type X = Promise<Promise<string>>;
@@ -20,8 +20,8 @@ export type equals = MyAwaited<X>;
 //Includes Type for Arrays with infer
 type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
   ? Equal<U, F> extends true
-    ? true
-    : Includes<R, U>
+  ? true
+  : Includes<R, U>
   : false;
 
 type Testit = Includes<["Kars", "Esidisi", "Wamuu"], "Wamuu">;
@@ -33,18 +33,18 @@ type MyParameters<T extends (...args: any[]) => any> = T extends (
   ? A
   : never;
 
-const foo = (arg1: string, arg2: number): void => {};
+const foo = (arg1: string, arg2: number): void => { };
 type check = MyParameters<typeof foo>;
 
 //Check first param
-type FirstParameter<T extends (...args: any[]) => any> = T extends (firstParam: infer A, ...args: any[]) => any ? A: never;
-type SecondParam<T extends (...args: any[]) => any> = T extends (firstParam: any, secondParam: infer A, ...args: any[]) => any ? A: never;
+type FirstParameter<T extends (...args: any[]) => any> = T extends (firstParam: infer A, ...args: any[]) => any ? A : never;
+type SecondParam<T extends (...args: any[]) => any> = T extends (firstParam: any, secondParam: infer A, ...args: any[]) => any ? A : never;
 type checkFirstParam = FirstParameter<typeof foo>;
 type checkSecondParam = SecondParam<typeof foo>;
 
 //Implementing ReturnType type with infer
-type MyReturnType<T> = T extends (...args: any[])=> infer R ? R : never;
-const tryFunction = ()=>true;
+type MyReturnType<T> = T extends (...args: any[]) => infer R ? R : never;
+const tryFunction = () => true;
 type OriginalReturnType = ReturnType<typeof tryFunction>;
 type MyOwnReturnType = MyReturnType<typeof tryFunction>
 
@@ -52,6 +52,24 @@ type MyOwnReturnType = MyReturnType<typeof tryFunction>
 //Chars check not only for spaces but also new lines and tabs
 type Chars = ' ' | '\n' | '\t';
 
-type Trim<S extends string | number> = S extends `${Chars}${infer SS}` ? Trim <SS> : S extends `${infer SS}${Chars}` ? Trim<SS> : S;
+type Trim<S extends string | number> = S extends `${Chars}${infer SS}` ? Trim<SS> : S extends `${infer SS}${Chars}` ? Trim<SS> : S;
 
 type Trimmed = Trim<' string '>
+
+// implement a Replace type for one occurence
+type Replace<S extends string, From extends string, To extends string> =
+  From extends '' ? S :
+  S extends `${infer Prefix}${From}${infer Suffix}` ? `${Prefix}${To}${Suffix}` :
+  S
+type ReplacedString = Replace<'foobar', 'bar', 'foo'>
+
+//implement a ReplaceAll for all occurences
+type ReplaceAll<S extends string, From extends string, To extends string> =
+  From extends '' ? S :
+  S extends `${infer Prefix}${From}${infer Suffix}` ? `${ReplaceAll<Prefix, From, To>}${To}${ReplaceAll<Suffix, From, To>}` :
+  S
+type ReplacedAllString = ReplaceAll<'foobarbar', 'bar', 'foo'> 
+
+// implement a StartsWith type for one occurence
+type StartsWith<T extends string, U extends string> = T extends `${U}${infer rest}` ? true : false;
+type StartsWithString = StartsWith<'foobar', 'foo'>
