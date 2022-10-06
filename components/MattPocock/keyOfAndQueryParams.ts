e226f9ed5fa0937d697098469db3c3f7519338da
Julian <julian.costinea@gmail.com>
2022-10-06 11:35:42 +0200

    more mattPocock

--- components/MattPocock/keyOfAndQueryParams.ts
+++ components/MattPocock/keyOfAndQueryParams.ts
@@ -1,19 +1,28 @@
-import {String, Union} from "ts-toolbelt";
+import { String, Union } from "ts-toolbelt";
 
 export interface Letters {
-    a: number,
-    b: string,
-    c: {
-        name: string
-    }
+  a: number;
+  b: string;
+  c: {
+    name: string;
+  };
 }
 
-type LetterValues = Letters[keyof Letters]
+type LetterValues = Letters[keyof Letters];
 
 const query = `/home?name=burke&profession=chat-baiter`;
 
 type Query = typeof query;
 
-type SecondQueryPart = String.Split<Query, "?">[1]
+type SecondQueryPart = String.Split<Query, "?">[1];
 
-type QueryElements = String.Split<SecondQueryPart, "&">
+type QueryElements = String.Split<SecondQueryPart, "&">;
+
+type QueryParams = {
+  [QueryElement in QueryElements[number]]: {
+    [Key in String.Split<QueryElement, "=">[0]]: String.Split<
+      QueryElement,
+      "="
+    >[1];
+  };
+}[QueryElements[number]];
